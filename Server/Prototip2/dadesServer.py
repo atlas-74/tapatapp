1a03b3198238119f8fcacc0b79089c6174e619df
atlas-74 <mohamedmitou74@gmail.com>
2025-02-25 14:50:17 +0100

    Update dadesServer.py

--- Server/Prototip2/dadesServer.py
+++ Server/Prototip2/dadesServer.py
@@ -18,15 +18,15 @@ users = [
 # Crear les classes Child, Tap, Role, Status i Treatment
 
 class Child:
-    def __init__(self, id, child_name, child_id, username):
-        self.id=id
-        self.child_name=child_name
-        self.child_id=child_id
-        self.username=username
+    def __init__(self, id, child_name, sleep_average, treatment_id, time):
+        self.id = id
+        self.child_name = child_name
+        self.sleep_average = sleep_average
+        self.treatment_id = treatment_id
+        self.time = time
 
     def __str__(self):
-        return "Child Id:" + str(self.child_id) + " Username:" + self.username
-
+        return "Child Id:" + str(self.id) + " Name:" + self.child_name
 
 children = [
     Child(id=1, child_name="Carol Child", sleep_average=8, treatment_id=1, time=6),
@@ -34,18 +34,21 @@ children = [
 ]
 
 class Tap:
-    def __init__(self, child_id, status_id):
+    def __init__(self, id, child_id, status_id, user_id, init, end):
+        self.id = id
         self.child_id = child_id
         self.status_id = status_id
+        self.user_id = user_id
+        self.init = init
+        self.end = end
     
     def __str__(self):
-        return " Child Id:" + str(self.child_id) + " Status Id:" + str(self.status_id)
+        return "Child Id:" + str(self.child_id) + " Status Id:" + str(self.status_id)
 
 taps = [
     Tap(id=1, child_id=1, status_id=1, user_id=1, init="2024-12-18T19:42:43", end="2024-12-18T20:42:43"),
     Tap(id=2, child_id=2, status_id=2, user_id=2, init="2024-12-18T21:42:43", end="2024-12-18T22:42:43")
 ]
-
 
 relation_user_child = [
     {"user_id": 1, "child_id": 1, "rol_id": 1},
@@ -55,12 +58,12 @@ relation_user_child = [
 ]
 
 class Role:
-    def __init__(self, username, type_rol):
-        self.username = username
+    def __init__(self, id, type_rol):
+        self.id = id
         self.type_rol = type_rol
 
     def __str__(self):
-        return "Username: " + str(self.username) + " Type Rol:" + self.type_rol
+        return "Role Id: " + str(self.id) + " Type Rol: " + self.type_rol
 
 roles = [
     Role(id=1, type_rol='Admin'),
@@ -77,26 +80,25 @@ class Status:
         self.status_id = status_id
 
     def __str__(self):
-        return "Status Id" + str(self.status_id) + " Name:" + self.name + " Child Id:" + str(self.child_id)
+        return "Status Id: " + str(self.status_id) + " Name: " + self.name + " Child Id: " + str(self.child_id)
     
 statuses = [
-    Status(id=1, name="sleep"),
-    Status(id=2, name="awake"),
-    Status(id=3, name="yes_eyepatch"),
-    Status(id=4, name="no_eyepatch")
+    Status(id=1, child_id=1, name="sleep", status_id=1),
+    Status(id=2, child_id=1, name="awake", status_id=2),
+    Status(id=3, child_id=2, name="yes_eyepatch", status_id=3),
+    Status(id=4, child_id=2, name="no_eyepatch", status_id=4)
 ]
 
 class Treatment:
-    def __init__(self, child_id, id, name):
+    def __init__(self, id, child_id, name):
         self.id = id
         self.child_id = child_id
         self.name = name
 
     def __str__(self):
-        return "Id:" + str(self.id) + " Name:" + self.name
+        return "Treatment Id: " + str(self.id) + " Name: " + self.name
 
 treatments = [
-    Treatment(id=1, name='Hour'),
-    Treatment(id=2, name='percentage')
+    Treatment(id=1, child_id=1, name='Hour'),
+    Treatment(id=2, child_id=2, name='Percentage')
 ]
-
